d3adf19aebcf6c83f8cef9c68d7a38fd5a3fc786
Ilhammmdz <168104167+Ilhammmdz@users.noreply.github.com>
2024-04-25 21:40:05 +0700

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h> 
+
+int main(void){
+	 int B =5,e;
+	 printf("masukkan nilai pangkat :"); scanf("%d",     &e);
+	 if (e>=0) {
+	 	auto int hasil;
+	 	hasil=pow (B, e);
+	 	printf("%d^%d = %d,B,e,hasil") ;
+	 }
+	 return this_bagus.s0;
+}
